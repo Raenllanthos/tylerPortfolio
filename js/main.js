2a6b7f48c4dc217e124759e2d5bb5cb6606b7305
raenllanthos <cwdarksouls@gmail.com>
2021-09-17 00:48:14 -0400

    Added third person controller. Doesn't work because javascript isn't reading correctly

--- js/main.js
+++ js/main.js
@@ -1,3 +1,428 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.132.2/build/three.module.js';
+
+import {FBXLoader} from 'https://cdn.jsdelivr.net/npm/three@0.132.2/examples/jsm/loaders/FBXLoader.js';
+
+import {OrbitControls} from 'https://cdn.jsdelivr.net/npm/three@0.132.2/examples/js/controls/OrbitControls.js';
+
+class BasicCharacterControllerProxy {
+    constructor(animations) {
+        this._animations = animations;
+    }
+
+    get animations() {
+        return this._animations;
+    }
+};
+
+class BasicCharacterController {
+    constructor(params) {
+        this._Init(params);
+    }
+
+    _Init(params) {
+        this._params = params;
+        this._decceleration = new THREE.Vector3(-0.0005, -0.0001, -5.0);
+        this._acceleration = new THREE.Vector3(1, 0.25, 50.0);
+        this._animations = {};
+        this._input = new BasicCharacterControllerInput();
+        this._stateMachine = new CharacterFSM(
+            new BasicCharacterControllerProxy(this._animations));
+        this._LoadModels();
+    }
+
+
+    _LoadModels() {
+        const loader = new FBXLoader();
+        loader.setPath('../resources/ybot/');
+        loader.load('ybot.fbx', (fbx) => {
+            fbx.scale.setScalar(0.1);
+            fbx.traverse(c => {
+                c.castShadow = true;
+            });
+            this._target = fbx;
+            this._params.scene.add(this._target);
+
+            this._mixer = new THREE.AnimationMixer(this._target);
+            this._manager = new THREE.LoadingManager();
+            this._manager.onLoad = () => {
+                this._stateMachine.SetState('idle');
+            };
+
+            const _OnLoad = (animName, anim) => {
+                const clip = anim.animations[0];
+                const action = this._mixer.clipAction(clip);
+                this._animations[animName] = {
+                    clip: clip,
+                    action: action,
+                };
+            };
+
+            const loader = new FBXLoader(this._manager);
+            loader.setPath('../resources/ybot/')
+            loader.load('walking.fbx', (a) => { _OnLoad('walking', a); });
+            loader.load('running.fbx', (a) => { _OnLoad('running', a); });
+            loader.load('idle.fbx', (a) => { _OnLoad('idle', a); });
+            loader.load('jump.fbx', (a) => { _OnLoad('jump', a); });
+        });
+    }
+
+    Update(timeInSeconds) {
+        if (!this._target) {
+            return;
+        }
+
+        this._stateMachine.Update(timeInSeconds, this._input);
+
+        const velocity = this._velocity;
+        const frameDecceleration = new THREE.Vector3(
+            velocity.x * this._decceleration.x,
+            velocity.y * this._decceleration.y,
+            velocity.z * this._decceleration.z
+        );
+        frameDecceleration.multiplyScalar(timeInSeconds);
+        frameDecceleration.z = Math.sign(frameDecceleration.z) * Math.min(
+            Math.abs(frameDecceleration.z), Math.abs(velocity.z));
+        velocity.add(frameDecceleration);
+        
+        const controlObject = this._target;
+        const _Quaternion = new THREE.Quaternion();
+        const _A = new THREE.Vector3();
+        const _Rotation = controlObject.quaternion.clone();
+
+        const acc = this._acceleration.clone();
+        if (this._input._keys.shift) {
+            acc.multiplyScalar(2.0);
+        }
+        if (this._stateMachine._currentState.Name == 'jump'){
+            acc.multiplyScalar(0.0);
+        }
+        if (this._input._keys.forward) {
+            velocity.z += acc.z * timeInSeconds;
+        }
+        if (this._input._keys.backward) {
+            velocity.z -= acc.z * timeInSeconds;
+        }
+        if (this._input._keys.left){
+            _A.set(0,1,0);
+            _Quaternion.setFromAxisAngle(_A, 4.0 * Math.PI * timeInSeconds * this._acceleration.y);
+            _Rotation.multiply(_Quaternion);
+        }
+        if (this._input._keys.right){
+            _A.set(0,1,0);
+            _Quaternion.setFromAxisAngle(_A, 4.0 * -Math.PI * timeInSeconds * this._acceleration.y);
+            _Rotation.multiply(_Quaternion);
+        }
+
+        controlObject.quaternion.copy(_Rotation);
+
+        const oldPosition = new THREE.Vector3();
+        oldPosition.copy(controlObject.position);
+
+        const forward = new THREE.Vector3(0,0,1);
+        forward.applyQuaternion(controlObject.quaternion);
+        forward.normalize();
+
+        const sideways = new THREE.Vector3(1,0,0);
+        sideways.applyQuaternion(controlObject.quaternion);
+        sideways.normalize();
+
+        sideways.multiplyScalar(velocity.x * timeInSeconds);
+        forward.multiplyScalar(velocity.z * timeInSeconds);
+
+        controlObject.position.add(forward);
+        controlObject.position.add(sideways);
+
+        oldPosition.copy(controlObject.position);
+
+        if (this._mixer) {
+            this._mixer.update(timeInSeconds);
+        }
+    }
+};
+
+class BasicCharacterControllerInput{
+    constructor() {
+        this._Init();
+    }
+
+    _Init() {
+        this._keys = {
+            forward: false,
+            backward: false,
+            left: false,
+            right: false,
+            space: false,
+            shift: false,
+        };
+        document.addEventListener('keydown', (e) => this._onKeyDown(e), false);
+        document.addEventListener('keyup', (e) => this._onKeyUp(e), false);
+    }
+    _onKeyDown(event) {
+        switch (event.keyCode) {
+            case 87: // W
+                this._keys.forward = true;
+                break;
+            case 65: // A
+                this._keys.left = true;
+                break;
+            case 83: // S
+                this._keys.backward = true;
+                break;
+            case 68: // D
+                this._keys.backward = true;
+                break;
+            case 32: // SPACE
+                this._keys.space = true;
+                break;
+            case 16: //SHIFT
+                this._keys.shift = true;
+                break;
+        }
+    }
+
+    _onKeyUp(event) {
+        switch (event.keyCode) {
+            case 87: // W
+                this._keys.forward = false;
+                break;
+            case 65: // A
+                this._keys.left = false;
+                break;
+            case 83: // S
+                this._keys.backward = false;
+                break;
+            case 68: // D
+                this._keys.backward = false;
+                break;
+            case 32: // SPACE
+                this._keys.space = false;
+                break;
+            case 16: //SHIFT
+                this._keys.shift = false;
+                break;
+        }
+    }
+};
+
+class FiniteStateMachine {
+    constructor() {
+        this._states = {};
+        this._currentState = null;
+    }
+
+    _AddState(name, type) {
+        this._states[name] = type;
+    }
+
+    SetState(name) {
+        const prevState = this._currentState;
+        if (prevState) {
+            if (prevState.Name == name) {
+                return;
+            }
+            prevState.Exit();
+        }
+        const state = new this._states[name](this);
+        this._currentState = state;
+        state.Enter(prevState);
+    }
+
+    Update(timeElapsed, input) {
+        if (this._currentState) {
+            this._currentState.Update(timeElapsed, input);
+        }
+    }
+};
+
+class CharacterFSM extends FiniteStateMachine {
+    constructor(proxy) {
+        super();
+        this._proxy = proxy;
+        this._Init();
+    }
+
+    _Init() {
+        this._AddState('idle', IdleState);
+        this._AddState('walking', WalkingState);
+        this._AddState('running', RunningState);
+        this._AddState('jump', JumpState);
+    }
+};
+
+class State {
+    constructor(parent) {
+        this._parent = parent;
+    }
+    Enter() {}
+    Exit() {}
+    Update() {}
+};
+
+class JumpState extends State {
+    constructor(parent) {
+        super(parent);
+        this._FinishedCallback = () => {
+            this._Finished();
+        }
+    }
+
+    get Name() {
+        return 'jump'
+    }
+
+    Enter(prevState) {
+        const currentAction = this._parent._proxy._animations['jump'].action;
+        const mixer = currentAction.getMixer();
+        mixer.addEventListenener("finished", this._FinishedCallback);
+
+        if (prevState) {
+            const prevAction = this._parent._proxy._animations[prevState.Name].action;
+            currentAction.reset();
+            currentAction.setLoop(THREE.LoopOnce, 1);
+            currentAction.clampWhenFinished = true;
+            currentAction.crossFadeFrom(prevAction, 0.5, true);
+            currentAction.play();
+        } else {
+            currentAction.play();
+        }
+    }
+
+    _Finished() {
+        this._Cleanup();
+        this._parent.SetState('idle');
+    }
+
+    _Cleanup() {
+        const action = this._parent._proxy._animations['jump'].action;
+        action.getMixer().removeEventListener('finished', this._CleanupCallback);
+    }
+
+    Exit() {
+        this._Cleanup();
+    }
+
+    Update(_) {}
+};
+
+class WalkingState extends State {
+    constructor(parent) {
+        super(parent);
+    }
+
+    get Name() {
+        return 'walking';
+    }
+
+    Enter(prevState) {
+        const currentAction = this._parent._proxy._animations['walking'].action;
+        if (prevState) {
+            const prevAction = this._parent._proxy.animations[prevState.Name].action;
+            currentAction.enabled = true;
+            if (prevState.Name == "running") {
+                const ratio = currentAction.getClip().duration / prevAction.getClip().duration;
+                currentAction.time = prevAction.time * ratio;
+            } else{ 
+                currentAction.time = 0.0;
+                currentAction.setEffectiveTimeScale(1.0);
+                currentAction.setEffectiveWeight(1.0);
+            }
+            currentAction.crossFadeFrom(prevAction, 0.5, true);
+            currentAction.play();
+        } else {
+            currentAction.play();
+        }
+    } // Enter
+
+    Exit() {}
+
+    Update(_, input) {
+        if (input._move.forward || input._move.backward) {
+            if (input._move.shift) {
+                this._parent.SetState('running');
+            }
+            return;
+        }
+    this._parent.SetState('idle');
+    } // input
+}; // State
+
+class RunningState extends State {
+    constructor(parent) {
+        super(parent);
+    }
+
+    get Name() {
+        return "running";
+    }
+
+    Enter(prevState) {
+        const currentAction = this._parent._proxy._animations['running'].action;
+        if (prevState) {
+            const prevAction = this._parent._proxy.animations[prevState.Name].action;
+            currentAction.enabled = true;
+            if (prevState.Name == 'walking') {
+                const ratio = currentAction.getClip().duration / prevAction.getClip().duration;
+                currentAction.time = prevAction.time * ratio;
+            } else{ 
+                currentAction.time = 0.0;
+                currentAction.setEffectiveTimeScale(1.0);
+                currentAction.setEffectiveWeight(1.0);
+            }
+            currentAction.crossFadeFrom(prevAction, 0.5, true);
+            currentAction.play();
+        } else {
+            currentAction.play();
+        }
+    } // Enter
+
+    Exit() {}
+
+    Update(_, input) {
+        if (input._move.forward || input._move.backward) {
+            if (!input._move.shift) {
+                this._parent.SetState('walking');
+            }
+            return;
+        }
+        this._parent.SetState('idle');
+    } // input
+}; // State
+
+class IdleState extends State {
+    constructor(parent) {
+        super(parent);
+    }
+
+    get Name() {
+        return 'idle';
+    }
+
+    Enter(prevState) {
+        const idleAction = this._parent._proxy._animations['idle'].action;
+        if (prevState) {
+            const prevAction = this._parent._proxy.animations[prevState.Name].action;
+            idleAction.time = 0.0;
+            idleAction.enabled = true;
+            idleAction.setEffectiveTimeScale(1.0);
+            idleAction.setEffectiveWeight(1.0);
+            idleAction.crossFadeFrom(prevAction, 0.5, true);
+            idleAction.play();
+        } else {
+            idleAction.play();
+        }
+    } // Enter
+
+    Exit() {}
+
+    Update(_, input) {
+        if (input._move.forward || input._move.backward) {
+            this._parent.SetState('walking');
+        } else if (input._move.space) {
+            this._parent.SetState('jump');
+        }
+    }
+};
+
 class World {
     constructor() {
         this._Initialize();
@@ -25,61 +450,94 @@ class World {
 
         this._scene = new THREE.Scene();
 
-        let light = new THREE.DirectionalLight(0xffffff);
-        light.position.set(20, 100, 10);
+        let light = new THREE.DirectionalLight(0xffffff, 1.0);
+        light.position.set(-100, 100, 100);
         light.target.position.set(0,0,0);
         light.castShadow = true;
-        light.shadow.bias = -0.01;
+        light.shadow.bias = -0.001;
         light.shadow.mapSize.width = 2048;
         light.shadow.mapSize.height = 2048;
         light.shadow.camera.near = 0.5;
         light.shadow.camera.far = 500;
-        light.shadow.camera.left = 100;
-        light.shadow.camera.right = -100;
-        light.shadow.camera.top = 100;
-        light.shadow.camera.bottom = -100;
+        light.shadow.camera.left = 50;
+        light.shadow.camera.right = -50;
+        light.shadow.camera.top = 50;
+        light.shadow.camera.bottom = -50;
         this._scene.add(light);
         
         light = new THREE.AmbientLight(0x404040);
         this._scene.add(light);
 
-        const controls = new THREE.OrbitControls(
+        const controls = new OrbitControls(
             this._camera, this._threejs.domElement);
-        controls.target.set(0, 20, 0);
+        controls.target.set(0, 10, 0);
         controls.update();
 
         const loader = new THREE.CubeTextureLoader();
         const texture = loader.load([
-            "./resources/skybox/left.jpg",
-            "./resources/skybox/right.jpg",
-            "./resources/skybox/top.jpg",
-            "./resources/skybox/bottom.jpg",
-            "./resources/skybox/back.jpg",
-            "./resources/skybox/front.jpg",
+            './resources/skybox/left.jpg',
+            './resources/skybox/right.jpg',
+            './resources/skybox/top.jpg',
+            './resources/skybox/bottom.jpg',
+            './resources/skybox/back.jpg',
+            './resources/skybox/front.jpg',
         ]);
         this._scene.background = texture;
 
         const plane = new THREE.Mesh(
-            new THREE.PlaneGeometry(100, 100, 1, 1),
+            new THREE.PlaneGeometry(100, 100, 10, 10),
             new THREE.MeshStandardMaterial({
-                color: 0xFFFFFF
+                color: 0x808080,
             }));
         plane.castShadow = false;
         plane.receiveShadow = true;
         plane.rotation.x = -Math.PI / 2;
         this._scene.add(plane);
 
-        const box = new THREE.Mesh(
-            new THREE.BoxGeometry(2,2,2),
-            new THREE.MeshStandardMaterial({
-                color: 0x808080
-            }));
-        box.position.set(0, 1, 0);
-        box.castShadow = true;
-        box.receiveShadow = true;
-        this._scene.add(box);
+        // const box = new THREE.Mesh(
+        //     new THREE.BoxGeometry(2,2,2),
+        //     new THREE.MeshStandardMaterial({
+        //         color: 0x808080
+        //     }));
+        // box.position.set(0, 1, 0);
+        // box.castShadow = true;
+        // box.receiveShadow = true;
+        // this._scene.add(box);
 
+        this._mixers = [];
+        this._previousRAF = null;
+        this._LoadAnimatedModel();
         this._RAF();
+    }
+
+    _LoadAnimatedModel() {
+        const params = {
+            camera: this._camera,
+            scene: this._scene,
+        }
+        this._controls = new BasicCharacterController(params);
+    }
+
+    _LoadAnimatedModelAndPlay(path, modelFile, animFile, offset) {
+        const loader = new FBXLoader();
+        loader.setPath(path);
+        loader.load(modelFile, (fbx) => {
+            fbx.scale.setScalar(0.1);
+            fbx.traverse(c => {
+                c.castShadow = true;
+            });
+            fbx.position.copy(offset);
+
+            const anim = new FBXLoader();
+            anim.setPath(path);
+            anim.load(animFile, (anim) => {
+                const m = new THREE.AnimationMixer(fbx);
+                this._mixers.push(m);
+                const idle = m.clipAction(anim.animations[0]);
+                idle.play();
+            });
+            this._scene.add(fbx);
+        });
     }
 
     _onWindowResize() {
@@ -88,10 +546,26 @@ class World {
         this._threejs.setSize(window.innerWidth, window.innerHeight);
     }
     _RAF() {
-        requestAnimationFrame(() => {
-            this._threejs.render(this._scene, this._camera);
+        requestAnimationFrame((t) => {
+            if (this._previousRAF === null) {
+                this._previousRAF = t;
+            }
             this._RAF();
+
+            this._threejs.render(this._scene, this._camera);
+            this._Step(t - this._previousRAF);
+            this._previousRAF = t;
         });
+    }
+
+    _Step(timeElapsed) {
+        const timeElapsedSeconds = timeElapsed * 0.001;
+        if (this._mixers) {
+            this._mixers.map(m => m.update(timeElapsedSeconds));
+        }
+        if (this._controls) {
+            this._controls.Update(timeElapsedSeconds);
+        }
     }
 }
 
